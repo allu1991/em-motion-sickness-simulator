6a28b2554cf105f71c095c8ac0140e918a75e5a3
allu1991 <aleksip@Aleksis-MacBook-Pro.local>
2024-10-09 09:37:35 +0300

    Add mobile disclaimer text and fullscreen mode

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
 window.addEventListener("load", function() {
+
+    const enterFullscreen = () => {
+        // Check for browser fullscreen support
+        if (typeof(document.body.requestFullscreen) !== 'undefined') {
+            if (!document.fullscreenElement || !document.webkitFullscreenElement) {
+                document.body.requestFullscreen();
+            }
+        } else {
+            console.error("Your browser does not support full screen mode")
+        }
+    };
+
     setTimeout ( function() {
 
         // Define DOM elements
         const theCanvas = document.querySelector("#the-canvas")
         const startButton = document.querySelector("#start-button")
+        const disclaimer = document.querySelector("#screen-disclaimer")
 
         // Start the app
         startButton.addEventListener("click", function() {
-            startButton.classList.add("hidden")
-            theCanvas.classList.remove("hidden");
+            enterFullscreen();
+            setTimeout ( function() {
+                startButton.classList.add("hidden")
+                theCanvas.classList.remove("hidden");
+                disclaimer.classList.add("hidden");
+            }, 100);
         })
 
         // Parallax
@@ -17,5 +34,5 @@ window.addEventListener("load", function() {
             selector: '.parallax-element'
         });
 
-    }, 100)
+    }, 100);
 })
